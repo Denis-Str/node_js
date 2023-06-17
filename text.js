a6264cdee8d4ec12c652373bd97a162acc155e7b
Denis-Str <web_life@inbox.ru>
2023-06-17 14:48:53 +0300

    git init

---
+++ text.js
@@ -0,0 +1,14 @@
+module.exports = {
+    text: `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Node JS</title>
+        </head>
+        <body>
+            <h4>Server init</h4>
+        </body>
+        </html>
+    `
+}
